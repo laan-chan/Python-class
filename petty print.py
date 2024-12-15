45968fa7afaeefd39698444c15765577b0b24532
laan-chan <laanchanboss@gmail.com>
2024-12-15 20:43:44 +0530

    Delete petty print.py

--- petty print.py
+++
@@ -1,7 +0,0 @@
-import pprint
-message="I am studying in BGSCET"
-count={}
-for character in message:
-    count.setdefault(character,0)
-    count[character]=count[character]+1
-pprint.pprint(count)
